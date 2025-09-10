8db5b9785c8538e0126654bd5e2af76d212be504
Art112-122 <a.zalutskij@gmail.com>
2025-09-10 19:51:49 +0300

    Комитттттттт

--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -1,9 +1,13 @@
 <script setup>
-import config from "../../config.json"
-import { useRoute } from "vue-router"
-import { ref } from "vue"
+import config from '../../config.json'
+import { useRoute } from 'vue-router'
+import { computed, ref } from 'vue'
 import router from 'src/router/index.js'
+import { useQuasar } from 'quasar'
+import { api } from 'boot/axios.js'
+import { getToken } from 'src/api/cookies.js'
 
+const $q = useQuasar()
 const route = useRoute()
 const pages = config[0]?.pages ?? {}
 
@@ -11,58 +15,244 @@ let foundGame = null
 
 for (const [key, game] of Object.entries(pages)) {
   if (game.pathName === route.params.game) {
-    foundGame = { key, ...game }
+    foundGame = {
+      key,
+      ...game,
+    }
     break
   }
 }
 
-if (foundGame === null) {
-  router.push({ name: 'profile'})
+if (foundGame === null || foundGame === undefined) {
+  router.push({ name: 'notFound' })
+}
+const modal = ref(false)
+const currentTab = ref(null)
+const categories = ref([])
+
+if (foundGame) {
+  categories.value = Object.entries(foundGame.categories).map(([label, value]) => ({
+    label,
+    value,
+  }))
+  currentTab.value = categories.value[0]?.value || null
 }
 const columns = ref([
-  { name: "name", label: "Обьявление", field: "name" },
-  { name: "author", label: "Автор", field: "author" }
+  { name: 'name', label: 'Обьявление', field: 'name' },
+  { name: 'author', label: 'Автор', field: 'author' },
 ])
 
-const rows = ref([
-  { name: "testname1", author: "authorname1" }
-])
+const rows = ref([{ name: 'testname1', author: 'authorname1', id: '1' }])
+
+function truncate(text, length = 30) {
+  if (!text) return ''
+  return text.length > length ? text.slice(0, length) + '...' : text
+}
+
+function routerPushTo() {
+  router.push(`/games/${route.params.game}/${currentTab.value}`)
+}
+
+const dialogSize = computed(() => {
+  let size = $q.screen.width
+  if (size > 1000) {
+    return 'width: 30vw'
+  } else if (size > 800) {
+    return 'width: 40vw'
+  } else {
+    return 'width: 70vw'
+  }
+})
+
+// -- Form --
+
+const name = ref('')
+const description = ref('')
+const game = ref(route.params.game)
+const selectedCategory = ref('')
+
+const options = computed(() => {
+  let option = []
+  categories.value.forEach((value) => option.push(value))
+  return option
+})
+
+const sendForm = () => {
+  api
+    .post(
+      '/games/add',
+      {
+        name: name.value,
+        description: description.value,
+        game: game.value,
+        category: selectedCategory.value,
+      },
+      {headers: {'Authorization': `Bearer ${getToken()}`}},
+    )
+    .then(() => {
+      modal.value = false
+    })
+    .catch((error) => {
+      if (error.status < 500) {
+        if (error.status === 422) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'Невалідна інформація, будь-ласка перевірте введені поля на правильність',
+          })
+        }
+        else if (error.response) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: error.response.data.error,
+          })
+        } else if (error.request) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'Невідома помилка, спробуйте пізніше',
+          })
+        } else {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'error',
+            message: 'Невідома помилка, спробуйте пізніше',
+          })
+        }
+      } else {
+        $q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'error',
+          message: 'Невідома помилка, спробуйте пізніше',
+        })
+      }
+    })
+}
 </script>
 
 <template>
   <q-tabs
-    style="margin-top: 3rem"
-    v-model="tab"
-    class="text-teal"
+    v-model="currentTab"
+    class="text-primary"
+    align="center"
+    @update:model-value="routerPushTo"
   >
-    <q-tab name="mails" icon="mail" label="Mails" />
-    <q-tab name="alarms" icon="alarm" label="Alarms" />
-    <q-tab name="movies" icon="movie" label="Movies" />
+    <q-tab v-for="cat in categories" :key="cat.value" :name="cat.value" :label="cat.label" />
   </q-tabs>
 
-  <q-table
-    flat bordered
-    :style="$q.screen.width < 750 ? 'margin: 3rem;': 'margin: 5rem;'"
-    :rows="rows"
-    :columns="columns"
-    no-data-label="Ничего не найдено:("
-    row-key="name"
-  >
-    <template v-slot:body="props">
-      <q-tr :props="props">
-        <q-td auto-width key="name" :props="props">
-          {{ props.row.name }}
-        </q-td>
-        <q-td auto-width key="author" :props="props">
-          {{ props.row.author }}
-          <q-avatar color="primary" size="24px">
-            {{ props.row.author[0].toUpperCase() }}
-          </q-avatar>
-        </q-td>
-      </q-tr>
-    </template>
-  </q-table>
+  <q-separator />
+
+  <div class="text-h3 text-white text-center text-bold">
+    {{ foundGame.key }}
+  </div>
+
+  <div class="game-image">
+    <img :src="`/cardimages/${foundGame.imagePath}`" alt="game image" />
+  </div>
+
+  <div :class="$q.screen.width < 750 ? 'margin-sm' : 'margin-md'">
+    <q-btn label="Створити" color="primary" @click="modal = true" />
+    <q-table
+      style="margin-top: 2rem"
+      flat
+      bordered
+      :rows="rows"
+      :columns="columns"
+      class="table-games"
+      no-data-label="Ничего не найдено:("
+      row-key="name"
+    >
+      <template v-slot:body="props">
+        <q-tr :props="props" v-on:click="router.push(`/post/view/${props.row.id}`)">
+          <q-td auto-width key="name" :props="props">
+            {{ truncate(props.row.name, 30) }}
+          </q-td>
+          <q-td auto-width key="author" :props="props">
+            {{ props.row.author }}
+            <q-avatar color="primary" size="24px">
+              {{ props.row.author[0].toUpperCase() }}
+            </q-avatar>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
+  </div>
+
+  <!-- Dialog -->
+  <q-dialog v-model="modal">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Створити</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section style="max-height: 50vh" class="scroll">
+        <q-input
+          :style="dialogSize"
+          filled
+          v-model="name"
+          label="Короткий опис \ Назва"
+          lazy-rules
+          maxlength="20"
+          :rules="[(val) => (val && val.length > 0) || 'Заповніть це поле будь ласка']"
+        />
+
+        <q-form>
+          <q-input
+            autogrow
+            filled
+            type="textarea"
+            v-model="description"
+            label="Повний опис"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || 'Заповніть це поле будь ласка']"
+          />
+          <q-input disable filled v-model="game" />
+          <q-select
+            label="Категорія"
+            transition-show="scale"
+            transition-hide="scale"
+            filled
+            v-model="selectedCategory"
+            :options="options"
+            :rules="[(val) => !!val || 'Оберіть категорію']"
+            style="width: 250px; margin-top: 1rem"
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions align="right">
+        <q-btn flat label="Закрити" color="primary" v-close-popup />
+        <q-btn flat label="Створити" v-on:click="sendForm" color="primary" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <style scoped>
+.margin-sm {
+  margin: 2rem 3rem 3rem;
+}
+
+.margin-md {
+  margin: 2rem 5rem 5rem;
+}
+
+.game-image img {
+  border-radius: 10%;
+  max-width: 100%;
+  height: auto;
+  object-fit: contain;
+  display: block;
+  margin: 0 auto;
+}
 </style>
